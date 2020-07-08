54e2dc7d3df58142cedbcec91bc6f16c6966d35e
Stephan <stephan.kop@outlook.com>
2020-07-08 23:11:04 +0200

    added hamburger menu

--- header.php
+++ header.php
@@ -3,10 +3,38 @@
         <a href="<?php echo home_url(); ?>">
         <h1>GameSource</h1>
         </a>
-        <nav>
+        <nav id="main-nav">
             <a id="Nieuws" href="<?php echo home_url(); ?>/nieuws">Nieuws</a>
             <a id="Reviews" href="<?php echo home_url(); ?>/reviews">Reviews</a>
             <a id="Previews" href="<?php echo home_url(); ?>/previews">Previews</a>
         </nav>
+        <div class="hamburger">
+        <!--    Made by Erik Terwan    -->
+        <!--   24th of November 2015   -->
+        <!--        MIT License        -->
+        <nav role="navigation">
+            <div id="menuToggle">
+                <input type="checkbox" id="checkbox"/>
+                <span id="span1"></span>
+                <span id="span2"></span>
+                <span id="span3"></span>
+                <ul id="menu">
+                    <div class="menu-wrapper">
+                    <?php get_search_form() ?>
+                    <div class="hamburger__links">
+                        <a href="<?php echo home_url(); ?>/nieuws"><li>Nieuws</li></a>
+                        <a href="<?php echo home_url(); ?>/reviews"><li>Reviews</li></a>
+                        <a href="<?php echo home_url(); ?>/previews"><li>Previews</li></a>
+                    <div class="hamburger__socials">
+                        <a href="twitter.com"><i class="fab fa-twitter"></i></a>
+                        <a href="facebook.com"><i class="fab fa-facebook-f"></i></a>
+                        <a href="instagram.com"><i class="fab fa-instagram"></i></a>
+                    </div>
+                    </div>
+                    </div>
+                </ul>
+            </div>
+        </nav>
+        </div>
     </div>
 </div>
