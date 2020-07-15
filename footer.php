7a585a7ad6f14273ae30f1b017fdbf919f86b5c3
Rowin206 <rowin.amsterdam@gmail.com>
2020-07-15 16:36:59 +0200

    Added definitive styling

--- footer.php
+++ footer.php
@@ -6,34 +6,35 @@
 
         <div class="mainfooter__column-container">
             <div class="mainfooter__column">
-                <h3 class="mainfooter__title">Menu</h3>
-                <a class="" href="<?php echo get_bloginfo("url")?>/test1">Nieuws</a>
-                <a class="" href="<?php echo get_bloginfo("url")?>/test2">Reviews</a>
-                <a class="" href="<?php echo get_bloginfo("url")?>/test3">Previews</a>
+                <h3 class="mainfooter__title">Sitemap</h3>
+                <a class="mainfooter__link" href="<?php echo get_bloginfo("url")?>/test1">Nieuws</a>
+                <a class="mainfooter__link" href="<?php echo get_bloginfo("url")?>/test2">Reviews</a>
+                <a class="mainfooter__link" href="<?php echo get_bloginfo("url")?>/test3">Previews</a>
             </div>
 
             <div class="mainfooter__column">
                 <h3 class="mainfooter__title">Terms</h3>
-                <a class="" href="<?php echo get_bloginfo("url")?>/test1">Algemene voorwaarden</a>
-                <a class="" href="<?php echo get_bloginfo("url")?>/test2">Privacyverklaring</a>
-                <a class="" href="<?php echo get_bloginfo("url")?>/test3">Cookies</a>
+                <a class="mainfooter__link" href="<?php echo get_bloginfo("url")?>/test1">Algemene voorwaarden</a>
+                <a class="mainfooter__link" href="<?php echo get_bloginfo("url")?>/test2">Privacyverklaring</a>
+                <a class="mainfooter__link" href="<?php echo get_bloginfo("url")?>/test3">Cookies</a>
             </div>
 
             <div class="mainfooter__column">
                 <h3 class="mainfooter__title">Contact</h3>
-                <h4>E-mail:</h4>
-                <a href="mailto:info@gamesource.nl">info@gamesource.nl</a>
-                <h4>Deel nieuws met ons:</h4>
-                <a href="/inform" class="">Nieuws doorgeven</a>
+                <a class="mainfooter__link" href="<?php echo get_bloginfo("url")?>/test1">Contact</a>
+                <h4 class="mainfooter__subtitle mainfooter__subtitle--margin-top">E-mail:</h4>
+                <a class="mainfooter__link" href="mailto:info@gamesource.nl">info@gamesource.nl</a>
+                <h4 class="mainfooter__subtitle mainfooter__subtitle--margin-top">Deel nieuws met ons:</h4>
+                <a href="/inform" class="mainfooter__link">Nieuws doorgeven</a>
             </div>
 
-            <div class="mainfooter__column">
+            <div class="mainfooter__column mainfooter__column--center">
                 <h3 class="mainfooter__title">Social Media</h3>
                 <div>
-                    <a href="https://www.facebook.com/flutcadeau" target="_blank"><i class="social-icon fab fa-facebook" aria-hidden="true"></i></a>
-                    <a href="https://www.instagram.com/flutcadeau" target="_blank"><i class="social-icon fab fa-instagram" aria-hidden="true"></i></a>
-                    <a href="https://www.twitter.com/flutcadeau" target="_blank"><i class="social-icon fab fa-twitter" aria-hidden="true"></i></a>
-                    <a href="https://www.pinterest.com/flutcadeau" target="_blank"><i class="social-icon fab fa-pinterest-p" aria-hidden="true"></i></a>
+                    <a class="mainfooter__link mainfooter__social-icon" href="https://www.facebook.com/flutcadeau" target="_blank"><i class="fab fa-facebook" aria-hidden="true"></i></a>
+                    <a class="mainfooter__link mainfooter__social-icon" href="https://www.instagram.com/flutcadeau" target="_blank"><i class="fab fa-instagram" aria-hidden="true"></i></a>
+                    <a class="mainfooter__link mainfooter__social-icon" href="https://www.twitter.com/flutcadeau" target="_blank"><i class="fab fa-twitter" aria-hidden="true"></i></a>
+                    <a class="mainfooter__link mainfooter__social-icon" href="https://www.pinterest.com/flutcadeau" target="_blank"><i class="fab fa-pinterest-p" aria-hidden="true"></i></a>
                 </div>
             </div>
         </div>
