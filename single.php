0c1a4b555b0cbb86408f7e8d83ed574bba200796
Rowin206 <rowin.amsterdam@gmail.com>
2020-07-20 17:55:05 +0200

    Added subtile styling changes

--- single.php
+++ single.php
@@ -8,43 +8,44 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script type="text/javascript">
-        let homeUrl = '<?php echo home_url(); ?>';
-        let userLoginStatus = "<?php echo $userLoginStatus ?>";
+    let homeUrl = '<?php echo home_url(); ?>';
+    let userLoginStatus = "<?php echo $userLoginStatus ?>";
     </script>
     <?php wp_head(); ?>
 </head>
 
-    <?php get_header(); ?>
+<?php get_header(); ?>
 
-    <?php
+<?php
     if (have_posts()) :
         while (have_posts()) : the_post(); ?>
-            <div class="featured-image">
-                <?php the_post_thumbnail('full'); ?>
+
+<div class="featured-image">
+    <?php the_post_thumbnail('full'); ?>
+</div>
+
+<div class="grid-container">
+    <div class="postContainer">
+        <div class="post">
+            <h2 class="post__title"><?php the_title(); ?></h2>
+            <div class="post__metadata">
+                <h3 class="post__author"><i class="fas fa-clock"></i><?php the_author(); ?></h3>
+                <h3 class="post__date"><i class="fas fa-comments"></i><?php the_date(); ?></h3>
             </div>
-    <div class="grid-container">
-        <div class="postContainer">
-            <div class="postContainer__metadata">
-                    <h2 class="metadata__title"><?php the_title(); ?></h2>
-                    <div class="metadata__author-time">
-                        <h3 class="metadata__detail"><?php the_author(); ?>&nbsp</h3>
-                        <h3 class="metadata__detail">| <?php the_date(); ?></h3>
-                    </div>
-                <div class="postContainer__content">
-                    <?php the_content(); ?>
-                </div>
+            <div class="post__content">
+                <?php the_content(); ?>
             </div>
-            <?php endwhile;
+        </div>
+
+        <?php endwhile;
             endif;
             ?>
 
-            <?php comments_template(); ?>
-
-        </div>
-
-        <div class="sideContainer">
-
-        </div>
+        <?php comments_template(); ?>
     </div>
+
+    <div class="sideContainer">
+    </div>
+</div>
 
 <?php get_footer(); ?>
