142f5bc8264d23e781e056dee6ad5e541620df8b
Stephan <stephan.kop@outlook.com>
2020-07-31 15:29:41 +0200

    added privacy page and fixed cookie glitch

--- template-sub-page.php
+++ template-sub-page.php
@@ -41,7 +41,9 @@ Template Name: Sub-Page
             </div>
         </div>
     </div>
-    <?php get_template_part( 'template-parts/cookie-message'); ?>
+    <div id="cookie-message">
+        <!--        --><?php //get_template_part( 'template-parts/cookie-message'); ?>
+    </div>
 </div>
 </div>
 
