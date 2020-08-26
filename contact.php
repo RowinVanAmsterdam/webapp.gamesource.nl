2c301ef9b599db8cbad316cad4c2cc51d45d207a
Rowin206 <rowin.amsterdam@gmail.com>
2020-08-26 15:42:56 +0200

    Added styling to contact page

--- contact.php
+++ contact.php
@@ -25,26 +25,33 @@ Template Name: Contact
 <body>
     <?php get_header(); ?>
 
+    <?php
+        if ( have_posts() ) :
+            while ( have_posts() ) : the_post();
+                the_content();
+            endwhile;
+        endif;
+        ?>
     <div class="container">
-        <div class="contact-container">
-            <div class="contact-container__contact-info">
+        <div class="contact-content">
+            <div class="contact-content__contact-info">
                 <img class="contact-info__logo"
                     src="<?php bloginfo('template_directory'); ?>/assets/img/gamesource--default.svg"
                     alt="GameSource logo">
                 <a class="contact-info__mail" href="mailto:info@gamesource.nl" target="_blank"><i
-                        class="far fa-envelope"></i>info@gamesource.nl</a>
-                <h2>Volg ons via:</h2>
-                <a class="" href="https://www.facebook.com/flutcadeau" target="_blank"><i class="fab fa-facebook"
+                        class="contact-info__mail-icon far fa-envelope"></i>info@gamesource.nl</a>
+                <h2 class="contact-info__social">Volg ons via:</h2>
+                <a class="contact-info__social-icon" href="https://www.facebook.com/flutcadeau" target="_blank"><i class="fab fa-facebook"
                         aria-hidden="true"></i></a>
-                <a class="" href="https://www.instagram.com/flutcadeau" target="_blank"><i class="fab fa-instagram"
+                <a class="contact-info__social-icon" href="https://www.instagram.com/flutcadeau" target="_blank"><i class="fab fa-instagram"
                         aria-hidden="true"></i></a>
-                <a class="" href="https://www.twitter.com/flutcadeau" target="_blank"><i class="fab fa-twitter"
+                <a class="contact-info__social-icon" href="https://www.twitter.com/flutcadeau" target="_blank"><i class="fab fa-twitter"
                         aria-hidden="true"></i></a>
-                <a class="" href="https://www.pinterest.com/flutcadeau" target="_blank"><i class="fab fa-pinterest-p"
+                <a class="contact-info__social-icon" href="https://www.pinterest.com/flutcadeau" target="_blank"><i class="fab fa-pinterest-p"
                         aria-hidden="true"></i></a>
             </div>
-            <div class="contact-container__form">
-                <form class="contact-form" action="" method="POST">
+            <div class="contact-content__form">
+                <!-- <form class="contact-form" action="" method="POST">
                     <h2 class="contact-form__title">Neem contact met ons op</h2>
                     <p class="contact-form__context">Contact met ons opnemen kan via het formulier hieronder, ons
                         antwoord zal je zo snel mogelijk op
@@ -65,7 +72,9 @@ Template Name: Contact
                     <div>
                         <button type="submit" name="contact-form__submit">Verstuur</button>
                     </div>
-                </form>
+                </form> -->
+                <?php get_template_part( 'template-parts/contact-form'); ?>
+
             </div>
         </div>
     </div>
